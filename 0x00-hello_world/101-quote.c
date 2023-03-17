4ec0b1d417f9b1b8415b7b97062bd30fe8f767a3
Ilyas Elkoudri <ilyasskodri@gmail.com>
2023-03-16 19:20:11 -0700

    task08

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <unistd.h>
-
+/**
+ * * main - Entry point
+ * *
+ * * Return: Always 1 (error code)
+ * */
 int main(void)
 {
-	  char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	    ssize_t len = sizeof(str) - 1;
-
-	      ssize_t bytes_written = write(STDERR_FILENO, str, len);
-	        if (bytes_written != len)
-			  {
-				      return 1;
-				        }
-
-		  return 0;
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			59);
+	return (1);
 }
