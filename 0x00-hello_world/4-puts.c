a3ae4db9d529e597e43cb70d6eb0d7211aef7b5d
Ilyas Elkoudri <ilyasskodri@gmail.com>
2023-03-16 18:39:00 -0700

    task04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,18 @@
+#include <unistd.h>
 #include <stdio.h>
 
-int main() {
-	    puts("Programming is like building a multilingual puzzle");
-	        return 0;
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0
+ *     */
+int main(void)
+{
+	    char *str = "Programming is like building a multilingual puzzle\n";
+
+	        /* Print the string using puts */
+	        puts(str);
+
+		    /* Return 0 to indicate successful execution */
+		    return (0);
 } 
