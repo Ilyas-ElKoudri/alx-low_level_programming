ef28b50f6bfd29352ef2639416c4a4bb20c5154e
Ilyas Elkoudri <ilyasskodri@gmail.com>
2023-03-17 18:33:30 -0700

    task08

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  * main - entry point
+ *   *
+ *    * print all single digit numbers using putchar
+ *     *
+ *      * Return: Always 0
+ *       */
+int main(void)
+{
+	int i;
+	char hex_digits[] = "0123456789abcdef";
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hex_digits[i]);
+	}
+	putchar('\n');
+	return (0);
+}
