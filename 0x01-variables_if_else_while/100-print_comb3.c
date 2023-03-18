78e66387c33ee326435f93707e70100804777d1d
Ilyas Elkoudri <ilyasskodri@gmail.com>
2023-03-17 18:37:45 -0700

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *  * main - Prints all possible combinations of two different dxgxts,
+ *   *   in ascending order, separated by a comma followed by a space.
+ *    *
+ *     * Return: Always 0.
+ *      */
+int main(void)
+{
+	int x, y;
+	for (x = 0; x < 9; x++)
+	{
+		for (y = x + 1; y <= 9; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+			if (x != 8 || y != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+			{
+				putchar('\n');
+			}
+		}
+	}
+	return (0);
+}
