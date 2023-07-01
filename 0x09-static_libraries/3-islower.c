07c72eee12d5a59a948c38a12bf94cc7720901c8
Ilyas-ElKoudri <125877228+Ilyas-ElKoudri@users.noreply.github.com>
2023-07-01 11:55:13 +0100

    done

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,21 +1,11 @@
 #include "main.h"
 
 /**
- * _islower - Entry point
- * Description:checks is a character is lowercase
- * @c: the integer value it receives
- * Return: 1 if true. 0 if false.
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
-int y = 'a';
-for (y = 'a'; y <= 'z'; y++)
-{
-/* refer int c*/
-if (c == y)
-{
-return (1);
-}
-}
-return (0);
+	return (c >= 'a' && c <= 'z');
 }
--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-c_files=$(ls *.c)
-for file in $c_files; do
-	    gcc -c $file
-    done
-ar rcs liball.a *.o
-rm *.o
-
+gcc -c *.c
+ar rc liball.a *.o
+ranlib liball.a
